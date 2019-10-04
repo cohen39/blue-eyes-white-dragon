05f7adb0c5378b74877a5127a818eec5dfeccc31
cohen39 <cohen39@purdue.edu>
2019-10-03 23:43:47 -0400

    WARNING: Constant motor force of 300 used for testing. Only other force implemented are aero forces. Other forces/moments are commented out. Aerodynamic forces are unstable and diverge. Implemented application of forces to body and rocket_motor links. Removed all other plugins that apply forces from the .sdf file. Rigidized fin joints. Changed rocket.launch so that --verbose is "true".

--- ros/src/rocket/src/RocketPlugin/RocketPlugin.cc
+++ ros/src/rocket/src/RocketPlugin/RocketPlugin.cc
@@ -33,7 +33,6 @@ GZ_REGISTER_MODEL_PLUGIN(RocketPlugin)
 ////////////////////////////////////////////////////////////////////////////////
 RocketPlugin::RocketPlugin()
   :
-	// _double_this(double_this_functions()),
   _rocket_aero_forces(rocket_aero_forces_functions()),
   _rocket_aero_moments(rocket_aero_moments_functions()),
   _rocket_prop_forces(rocket_prop_forces_functions()),
@@ -47,15 +46,26 @@ RocketPlugin::RocketPlugin()
   _rocket_prop_forces.arg (2,p); //Set rocket constant parameters for each eom
   _rocket_prop_moments.arg(2,p); //Set rocket constant parameters for each eom
 
-  _rocket_aero_forces.res(0,FA_b); //Set rocket aero forces
+  _rocket_aero_forces.arg (0,x); //Set address of rocket state for each eom
+  _rocket_aero_moments.arg(0,x); //Set address of rocket state for each eom
+  _rocket_prop_forces.arg (0,x); //Set address of rocket state for each eom
+  _rocket_prop_moments.arg(0,x); //Set address of rocket state for each eom
+
+  //TODO implement control algorithm that determines u. Make u a private member variable and update it in update().
+  double u[4]   = {0,0,0,0};     //Set input to zero for now
+  _rocket_aero_forces.arg (1,u); 
+  _rocket_aero_moments.arg(1,u); 
+  _rocket_prop_forces.arg (1,u); 
+  _rocket_prop_moments.arg(1,u); 
+
+  _rocket_aero_forces.res (0,FA_b); //Set rocket aero forces
   _rocket_aero_moments.res(0,MA_b); //Set rocket aero moments
-  _rocket_prop_forces.res(0,FP_b); //Set rocket propulsion forces
+  _rocket_prop_forces.res (0,FP_b); //Set rocket propulsion forces
   _rocket_prop_moments.res(0,FP_b); //Set rocket propulsion moments
 
-  _quat2mrp.res(0,r_nb);  //mrp
-  _tf_linvel.res(0,v_b);  //transfer frame
-  _tf_angvel.res(0,omg_b);//transfer frame
-
+  _quat2mrp.res (0,r_nb ); //mrp
+  _tf_linvel.res(0,v_b  ); //transfer frame
+  _tf_angvel.res(0,omg_b); //transfer frame
 }
 
 /////////////////////////////////////////////////
@@ -115,15 +125,16 @@ void RocketPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
   GZ_ASSERT(_sdf, "RocketPlugin _sdf pointer is NULL");
   this->model = _model;
 
+    // Find body link to apply aero forces
+  if (!this->FindLink("body", _sdf, this->body)) {
+    GZ_ASSERT(false, "RocketPlugin failed to find body");
+  }
+
   // Find motor link to apply propulsion forces
   if (!this->FindLink("motor", _sdf, this->motor)) {
     GZ_ASSERT(false, "RocketPlugin failed to find motor");
   }
   
-  // Find body link to apply aero forces
-  if (!this->FindLink("body", _sdf, this->body)) {
-    GZ_ASSERT(false, "RocketPlugin failed to find body");
-  }
 
   // Update time.
   this->lastUpdateTime = this->model->GetWorld()->SimTime();
@@ -145,35 +156,36 @@ void RocketPlugin::Update(const common::UpdateInfo &/*_info*/)
 
   if (curTime > this->lastUpdateTime)
   {
-    double dt = (curTime - this->lastUpdateTime).Double();
+    double dt           = (curTime - this->lastUpdateTime).Double();
 
     // ref to definition of Model.hh to get parameters
-    auto motor_inertial = this->motor->GetInertial();
-    float m_fuel = motor_inertial->Mass();
-    float m_dot = 0.1;
-    float m_empty = 0.0;
-    m_fuel = m_fuel - m_dot*dt; // x[13]
+    auto  motor_inertial= this->motor->GetInertial();
+    float m_fuel        = motor_inertial->Mass();
+    float m_dot         = 0.1;
+    float m_empty       = 0.0;
+          m_fuel        = m_fuel - m_dot*dt; // x[13]
   
-    float ve = this->p[6]; // exit velocity, m/s, guess
+    float ve            = this->p[6];        // exit velocity, m/s, guess
     
     // Get current states from gazebo
-    auto pose_world_inertia = this->body->WorldInertialPose(); //pose in world inertia frame ????
-    auto pos_ine = pose_world_inertia.Pos(); // [x,y,z], use [] to access elements
-    auto rot_ine = pose_world_inertia.Rot(); // [w x y z], quaternions. use .qw .qx .qy .qz to access.
+    auto pose_world_inertia = this->body->WorldInertialPose();  //pose in world inertia frame ????
+    auto pos_ine            = pose_world_inertia.Pos();         // [x,y,z], use [] to access elements
+    auto rot_ine            = pose_world_inertia.Rot();         // [w x y z], quaternions. use .qw .qx .qy .qz to access.
 
-    auto omega_ine = this->body->WorldAngularVel(); // inertial angular velocity
-    auto vel_ine = this->body->WorldLinearVel(); // inertial linaer velocity
+    auto omega_ine          = this->body->WorldAngularVel();    // inertial angular velocity
+    auto vel_ine            = this->body->WorldLinearVel();     // inertial linaer velocity
     
     // std::cout<<"\n Inertial Velocity"<<std::endl;
     // std::cout<<vel_ine<<std::endl;
 
-    // TODO: Convert inertial frame velocities to body frame.
-    double pos_n[3] = {pos_ine.X(),pos_ine.Y(),pos_ine.Z()};
-    double q[4] = {rot_ine.W(), rot_ine.X(), rot_ine.Y(), rot_ine.Z()};
-    double v_ine[3] = {vel_ine.X(),vel_ine.Y(),vel_ine.Z()};
-    double omg_ine[3] = {omega_ine.X(), omega_ine.Y(), omega_ine.Z()};
+    double pos_n[3]   = {pos_ine.X()  ,pos_ine.Y()  ,pos_ine.Z()  };
+    double q[4]       = {rot_ine.W()  ,rot_ine.X()  ,rot_ine.Y()  ,rot_ine.Z()};
+    double v_ine[3]   = {vel_ine.X()  ,vel_ine.Y()  ,vel_ine.Z()  };
+    double omg_ine[3] = {omega_ine.X(),omega_ine.Y(),omega_ine.Z()};
 
-    _quat2mrp.arg(0,q);
+    //Convert velocities wrt inertial frame to velocities wrt body frame
+    //(It is also necessary to convert orientation from world to local csys using modified rodrigues parameters)
+    _quat2mrp.arg (0,q);
     _tf_linvel.arg(0,v_ine);
     _tf_linvel.arg(1,q);
     _tf_angvel.arg(0,omg_ine);
@@ -181,28 +193,32 @@ void RocketPlugin::Update(const common::UpdateInfo &/*_info*/)
 
     _tf_linvel.eval(); //v_b
     _tf_angvel.eval(); //omega_b
-    _quat2mrp.eval(); //r_nb
+    _quat2mrp.eval (); //r_nb
     
-  
-    double *x = new double[13];
-    std::copy(omg_b,omg_b+3,x);
-    std::copy(r_nb,r_nb+4,x+3);
-    std::copy(v_b,v_b+3,x+7);
-    std::copy(pos_n,pos_n+3,x+10);
-    x[13] = m_fuel;
+    //Fill x array with states (wrt body frame)
     //{omg_b, r_nb, v_b, pos_n, m_fuel}
+    std::copy(omg_b, omg_b+3, x   );
+    std::copy(r_nb , r_nb +4, x+3 );
+    std::copy(v_b  , v_b  +3, x+7 );
+    std::copy(pos_n, pos_n+3, x+10);
+    x[13] = m_fuel;    
+
+    _rocket_aero_forces.eval (); //Calculate new value of FA_b
+    _rocket_aero_moments.eval(); //Calculate new value of MA_b
+    _rocket_prop_forces.eval (); //Calculate new value of FP_b
+    _rocket_prop_moments.eval(); //Calculate new value of MP_b
 
 
-    // TODO: Set states x and inputs u
-    double u[3] = {0};
+    //TODO: Make sure local csys convention in Gazebo is consistent with what we used for formulation of casadi functions
+    //Apply updated forces and moments to rocket body and motor in gazebo
+    this->motor->AddRelativeForce (ignition::math::Vector3d(0,0,300));
+    //this->motor->AddRelativeForce (ignition::math::Vector3d(FP_b[0], FP_b[1], FP_b[2]));
+    //gzdbg << "FP_b[0]=" << FP_b[0] << " FP_b[1]=" << FP_b[1] << " FP_b[2]=" << FP_b[2] << "\n" << std::endl;
+    this->body ->AddRelativeForce (ignition::math::Vector3d(FA_b[0], FA_b[1], FA_b[2]));
+    gzdbg << "FA_b[0]=" << FA_b[0] << " FA_b[1]=" << FA_b[1] << " FA_b[2]=" << FA_b[2] << "\n" << std::endl;
+    //this->motor->AddRelativeTorque(ignition::math::Vector3d(MP_b[0], MP_b[1], MP_b[2]));
+    this->body ->AddRelativeTorque(ignition::math::Vector3d(MA_b[0], MA_b[1], MA_b[2]));
 
-    
-    FP_b[2] = 10;
-    
-    // TODO: Add forces and moments to relative parts
-    this->motor->AddRelativeForce(ignition::math::Vector3d(FP_b[0], FP_b[1],FP_b[2]));
-    this->body->AddRelativeForce(ignition::math::Vector3d(FA_b[0], FA_b[1],FA_b[2]));
-    
     motor_inertial->SetMass(m_fuel);
     motor_inertial->SetInertiaMatrix(0, 0, 0, 0, 0, 0); // treat as point mass
     this->lastUpdateTime = curTime;

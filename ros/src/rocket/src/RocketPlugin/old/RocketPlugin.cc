61d1a6d97ff9723d0697cef98e8467e8dc5c4785
zp-yang <yang2zhanpeng@gmail.com>
2019-10-08 16:57:10 -0400

    updated the plugin, allowing parameters to be modified in the .sdf model file.

--- ros/src/rocket/src/RocketPlugin/old/RocketPlugin.cc
+++ ros/src/rocket/src/RocketPlugin/old/RocketPlugin.cc
@@ -17,7 +17,6 @@
 
 #include <functional>
 #include <string>
-#include "math.h"
 #include <sdf/sdf.hh>
 #include <gazebo/common/Assert.hh>
 #include <gazebo/common/Plugin.hh>
@@ -25,7 +24,6 @@
 #include <gazebo/physics/physics.hh>
 #include <gazebo/transport/transport.hh>
 #include "RocketPlugin.hh"
-#include <vector>
 
 
 using namespace gazebo;
@@ -33,10 +31,16 @@ using namespace gazebo;
 GZ_REGISTER_MODEL_PLUGIN(RocketPlugin)
 
 ////////////////////////////////////////////////////////////////////////////////
-RocketPlugin::RocketPlugin():
-    rocket_force_moment(rocket_force_moment_functions())
+RocketPlugin::RocketPlugin()
+  :
+  _rocket_force_moment(rocket_force_moment_functions())
 {
-    std::cout << "hello rocket plugin" << std::endl;
+  _rocket_force_moment.arg (0,x_gzb); //Set address of rocket state for each eom
+  _rocket_force_moment.arg (1,u); 
+  _rocket_force_moment.arg (2,p); //Set rocket constant parameters for each eom
+
+  _rocket_force_moment.res (0,F_b); //Set rocket forces
+  _rocket_force_moment.res (1,M_b); //Set rocket moments
 }
 
 /////////////////////////////////////////////////
@@ -96,69 +100,15 @@ void RocketPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
   GZ_ASSERT(_sdf, "RocketPlugin _sdf pointer is NULL");
   this->model = _model;
 
-  // Find body link to apply
+    // Find body link to apply forces and moments
   if (!this->FindLink("body", _sdf, this->body)) {
     GZ_ASSERT(false, "RocketPlugin failed to find body");
   }
 
-  std::vector<std::string> params = 
-  {
-    "g",
-    "Jx",
-    "Jy",
-    "Jz",
-    "Jxz",
-    "ve",
-    "l_fin",
-    "CL_a",
-    "CL_0",
-    "CD_0",
-    "K",
-    "s_fin",
-    "rho",
-    "m_empty",
-    "l_motor"
-  };
-
-  // set parameters from sdf
-  for (int i=0;i < params.size();i++)
-  {
-    if (_sdf->HasElement(params[i]))
-    {
-      p[i] = _sdf->Get<double>(params[i]);
-      gzdbg <<params[i] <<": " << p[i] <<std::endl;
-    }
-    else
-    {
-      gzdbg << "Couldn't find <" << params[i] << ">"<<std::endl;
-    }
-  }
-
-  // set mass flow rate
-  if (_sdf->HasElement("m_dot"))
-  {
-    m_dot = _sdf->Get<double>("m_dot");
-  }
-  else
-  {
-    gzdbg << "Couldn't find <m_dot>, using default value" << std::endl;
-  }
-
-  // set total mass
-  double m_total = 0.4;
-  if (_sdf->HasElement("m_total"))
-  {
-    m_total = _sdf->Get<double>("m_total");
-  }
-  else
-  {
-    gzdbg << "Couldn't find <m_total>, using default value" << std::endl;
-  }
-
   // Disable gravity, we will handle this in plugin
   this->body->SetGravityMode(false);
-  this->body->GetInertial()->SetMass(m_total); //  set your total mass with fuel here
-
+  this->body->GetInertial()->SetMass(0.4); //  set your total mass with fuel herethis->body->SetGravityMode(false);
+  
   // Update time.
   this->lastUpdateTime = this->model->GetWorld()->SimTime();
 
@@ -179,80 +129,69 @@ void RocketPlugin::Update(const common::UpdateInfo &/*_info*/)
 
   if (curTime > this->lastUpdateTime)
   {
-    double dt = (curTime - this->lastUpdateTime).Double();
-    auto inertial = this->body->GetInertial();
+    double dt           = (curTime - this->lastUpdateTime).Double();
 
-    // state
-    double m = inertial->Mass();
-
-    // parameters
-    double Jx = p[1];
-    double Jy = p[2];
-    double Jz = p[3];
-    double l_fin = p[6];
-    double m_empty = p[13];
-    double l_motor = p[14];
-
-    // integration for rocket mass flow
-    m -= m_dot*dt;
-    if (m < m_empty) {
-      m = m_empty;
-      m_dot = 0;
+    // ref to definition of Model.hh to get parameters
+    auto  rocket_inertial= this->body->GetInertial();
+    float m_empty = p[13];
+    float m_total = rocket_inertial->Mass();
+    float m_fuel  = m_total - m_empty;
+    float m_dot   = u[0];
+    gzdbg << "\nm_empty = " << m_empty <<std::endl << "m_total = " << m_total <<std::endl;
+    m_fuel = m_fuel - m_dot*dt; // x[13]
+    if (m_fuel <= 0)
+    {
+      m_fuel = 0;
+      u[0] = 0;
     }
-    float m_fuel = m - m_empty;
-    inertial->SetMass(m);
-    inertial->SetInertiaMatrix(Jx + m_fuel*l_motor*l_motor, Jy + m_fuel*l_motor*l_motor, Jz, 0, 0, 0);
+    float Jx = p[1];
+    float Jy = p[2];
+    float Jz = p[3];
+    float Jxz = p[4];
+    float l_motor = p[15];
+    rocket_inertial->SetMass(m_fuel+m_empty);
+    rocket_inertial->SetInertiaMatrix(Jx + m_fuel*l_motor*l_motor, Jy+m_fuel*l_motor*l_motor, Jz, 0, 0, 0); // treat as point mass
     this->lastUpdateTime = curTime;
+        
+    // Get current states from gazebo
+    auto pose_world_inertia = this->body->WorldPose();  //pose in world inertia frame ????
+    auto pos_ENU            = pose_world_inertia.Pos();         // [x,y,z], use [] to access elements
+    auto rot_ENU            = pose_world_inertia.Rot();         // [w x y z], quaternions. use .qw .qx .qy .qz to access.
 
-    auto vel_ENU = this->body->RelativeLinearVel();
-    auto omega_ENU = this->body->RelativeAngularVel();
-    auto pose = this->body->WorldPose();
-    auto q_ENU_FLT = pose.Rot();
-    auto pos_ENU = pose.Pos();
+    auto omega_ENU          = this->body->RelativeAngularVel();    // inertial angular velocity
+    auto vel_ENU            = this->body->RelativeLinearVel();     // inertial linaer velocity
 
-    gzdbg<<"vel_enu = "<<vel_ENU<<std::endl;
-    gzdbg<<"omega_enu = "<<omega_ENU<<std::endl;
+    double pos_n[3]   = {pos_ENU.X()  ,pos_ENU.Y()  ,pos_ENU.Z()  };
+    double q[4]       = {rot_ENU.W()  ,rot_ENU.X()  ,rot_ENU.Y()  ,rot_ENU.Z()};
+    double v_ENU[3]   = {vel_ENU.X()  ,vel_ENU.Y()  ,vel_ENU.Z()  };
+    double omg_ENU[3] = {omega_ENU.X(),omega_ENU.Y(),omega_ENU.Z()};
 
-    double x[14] = {
-      omega_ENU.X(), omega_ENU.Y(), omega_ENU.Z(),
-      q_ENU_FLT.W(), q_ENU_FLT.X(), q_ENU_FLT.Y(), q_ENU_FLT.Z(),
-      vel_ENU.X(), vel_ENU.Y(), vel_ENU.Z(),
-      pos_ENU.X(), pos_ENU.Y(), pos_ENU.Z(),
-      m_fuel};
+    //Convert velocities wrt inertial frame to velocities wrt body frame
+    //(It is also necessary to convert orientation from world to local csys using modified rodrigues parameters)
+    
+    //Fill x array with states (ENU frame)
+    //{omg_b, r_nb, v_b, pos_n, m_fuel}
+    std::copy(omg_ENU, omg_ENU+3, x_gzb   );
+    std::copy(q , q+4, x_gzb+3 );
+    std::copy(v_ENU  , v_ENU  +3, x_gzb+7 );
+    std::copy(pos_ENU, pos_ENU+3, x_gzb+10);
+    x_gzb[13] = m_fuel;
 
-    double u[4] = {m_dot, 0, 0, 0};
-    double F_FLT[3] = {0, 0, 0};
-    double M_FLT[3] = {0, 0, 0};
+    _rocket_force_moment.eval (); //Calculate new value of F_b and M_b
 
-    rocket_force_moment.arg(0, x);
-    rocket_force_moment.arg(1, u);
-    rocket_force_moment.arg(2, p);
-    rocket_force_moment.res(0, F_FLT);
-    rocket_force_moment.res(1, M_FLT);
-    rocket_force_moment.eval();
+    //TODO: Make sure local csys convention in Gazebo is consistent with what we used for formulation of casadi functions
+    //Apply updated forces and moments to rocket body and motor in gazebo
+    
+    gzdbg << "M_fuel = " << m_fuel << std::endl;
+    gzdbg << "F_F=" << F_b[0] << " F_L=" << F_b[1] << " F_B=" << F_b[2] << std::endl;
+    gzdbg << "M_F=" << M_b[0] << " M_L=" << M_b[1] << " M_B=" << M_b[2] << std::endl;
 
-    // debug
-    // for (int i=0; i<14; i++) {
-    //   gzdbg << "x[" << i << "]: " << x[i] << "\n";
-    // }
-    // for (int i=0; i<4; i++) {
-    //   gzdbg << "u[" << i << "]: " << u[i] << "\n";
-    // }
-    for (int i=0; i<15; i++) {
-      gzdbg << "p[" << i << "]: " << p[i] << "\n";
-    }
-    gzdbg << std::endl;
-    gzdbg << "mass: " << m << std::endl;
-    gzdbg << "force: " << F_FLT[0] << " " << F_FLT[1] << " " << F_FLT[2] << std::endl;
-    gzdbg << "moment: " << M_FLT[0] << " " << M_FLT[1] << " " << M_FLT[2] << std::endl;
-
-    // chceck that forces/moments finite before we apply them
     for (int i=0; i<3; i++) {
-      GZ_ASSERT(isfinite(F_FLT[i]), "non finite force");
-      GZ_ASSERT(isfinite(M_FLT[i]), "non finite moment");
+      GZ_ASSERT(isfinite(F_b[i]), "non finite force");
+      GZ_ASSERT(isfinite(M_b[i]), "non finite moment");
     }
 
-    this->body->AddRelativeForce(ignition::math::v4::Vector3d(F_FLT[0], F_FLT[1], F_FLT[2]));
-    this->body->AddRelativeTorque(ignition::math::v4::Vector3d(M_FLT[0], M_FLT[1], M_FLT[2]));
+    this->body ->AddRelativeForce (ignition::math::Vector3d(F_b[0], F_b[1], F_b[2]));
+    this->body ->AddRelativeTorque(ignition::math::Vector3d(M_b[0], M_b[1], M_b[2]));
   }
 }
--- ros/src/rocket/src/RocketPlugin/old/RocketPlugin.hh
+++ ros/src/rocket/src/RocketPlugin/old/RocketPlugin.hh
@@ -29,12 +29,45 @@
 #include <gazebo/msgs/msgs.hh>
 #include <gazebo/physics/PhysicsTypes.hh>
 #include <gazebo/transport/TransportTypes.hh>
+#include "rocket.pb.h"
 #include "casadi/CasadiFunc.hpp"
 #include "casadi_gen.h"
+#include <math.h>
+
 
 namespace gazebo
 {
 
+  typedef const boost::shared_ptr<const msgs::Rocket> ConstRocketPtr;
+  /// \brief Allow moving the control surfaces of a Rocket plane. This
+  /// plugin might be used with other models that have similar control surfaces.
+  ///
+  /// The plugin requires the following parameters:
+  /// <propeller>         Name of the joint controlling the propeller spin.
+  /// <propeller_max_rpm> Maximum angular speed in rpm.
+  /// <left_aileron>      Name of the joint controlling the left aileron.
+  /// <left_flap>         Name of the joint controlling the left flap.
+  /// <right_aileron>     Name of the joint controlling the right aileron.
+  /// <right_flap>        Name of the joint controlling the right flap.
+  /// <elevators>         Name of the joint controlling the rear elevators.
+  /// <rudder>            Name of the joint controlling the rudder.
+  ///
+  /// The following parameters are optional:
+  /// <propeller_p_gain> P gain for the PID that controls the propeller's speed.
+  /// <propeller_i_gain> I gain for the PID that controls the propeller's speed.
+  /// <propeller_d_gain> D gain for the PID that controls the propeller's speed.
+  /// <surfaces_p_gain> P gain for the PID that controls the position of the
+  ///                   control surfaces.
+  /// <surfaces_i_gain> I gain for the PID that controls the position of the
+  ///                   control surfaces.
+  /// <surfaces_d_gain> D gain for the PID that controls the position of the
+  ///                   control surfaces.
+  ///
+  /// The plugin will be subscribed to the following topic:
+  /// "~/<model_name>/control" The expected value is a Rocket message.
+  ///
+  /// The plugin will advertise the following topic with the current state:
+  /// "~/<model_name>/state"
   class GAZEBO_VISIBLE RocketPlugin : public ModelPlugin
   {
     /// \brief Constructor.
@@ -79,6 +112,9 @@ namespace gazebo
     /// \brief Pointer to the model;
     private: physics::ModelPtr model;
 
+    /// \brief Motor Link
+    private: physics::LinkPtr body;
+
     /// \brief keep track of controller update sim-time.
     private: gazebo::common::Time lastUpdateTime;
 
@@ -94,14 +130,14 @@ namespace gazebo
     /// \brief Ignition Publisher.
     private: ignition::transport::Node::Publisher statePubIgn;
 
-    /// \brief Motor Link
-    private: physics::LinkPtr body;
+    /// \brief Our casadi function
+      private: CasadiFunc _rocket_force_moment;
 
-	  /// \brief Our casadi function
-    private: CasadiFunc rocket_force_moment;
+    /// Forces and Moments
+      private: double F_b[3]={0,0,0}, M_b[3]={0,0,0};
 
-    /// \default parameters
-    private: double p[15]= {
+    /// Parameters of the rocket
+      private: double p[15]= {
         9.81,         //gravitational constant
         1.0,            //Jxx
         1.0,            //Jyy
@@ -109,7 +145,7 @@ namespace gazebo
         0.1,          //Jxz
         350.0,          //Ve
         1,          //l_fin
-        2*3.1415926535,     //C_L_alpha
+        2*M_PI, //C_L_alpha
         0.0,            //C_L_0
         0.01,         //C_D_0
         0.01,         //K
@@ -118,7 +154,12 @@ namespace gazebo
         0.2,          //m_empty
         1.0           //l_motor
       };
-    private: double m_dot = 0.1;
+
+      /// states
+      private: double x_gzb[14] = {0};
+      
+      /// inputs
+      private: double u[4]      = {0.1,0,0,0};     //Set input to zero for now
   };
 }
 #endif
